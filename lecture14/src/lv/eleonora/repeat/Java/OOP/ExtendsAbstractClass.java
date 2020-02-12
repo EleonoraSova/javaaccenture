0cba796c21f0819a12275874ac2df5efd65318ab
Eleonora <eleonorasova@gmail.com>
2020-02-12 21:29:30 +0200

    lecture8

---
+++ lecture14/src/lv/eleonora/repeat/Java/OOP/ExtendsAbstractClass.java
@@ -0,0 +1,8 @@
+package lv.eleonora.repeat.Java.OOP;
+
+public class ClassExtendsAbstractClass extends AbstractClass {
+
+	
+	}
+
+}
---
+++ lecture14/src/lv/eleonora/repeat/Java/OOP/repeatJavaClass.java
@@ -0,0 +1,13 @@
+package lv.eleonora.repeat.Java.OOP;
+
+abstract class repeatJavaClass {
+
+	public abstract void abstractMethod();
+	
+	public void sleep(){
+		System.out.println("Zzzzzz");
+		
+
+	}
+
+}
